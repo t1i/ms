a29be8252082c6f35f3c6aa5c79c4f286fef06ac
t1j <85011304+t1j@users.noreply.github.com>
2021-06-22 17:35:21 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+    window.onload=()=>{
+      function adcode(njs,vid) {
+        document.querySelectorAll(njs).forEach((ee) => {
+          if (getComputedStyle(ee).display!='none') {
+            ee.innerHTML=`<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-8870343551793834" data-ad-slot=${vid} data-ad-format="auto" data-full-width-responsive="true"></ins>`
+            let spt = document.createElement('script');
+            spt.text = `(adsbygoogle=window.adsbygoogle||[]).push({});`;
+            ee.append(spt);
+          }
+        })
+      }
+      adcode('hrads','4437257043')
+      adcode('vrads','9174358269')
+    }
