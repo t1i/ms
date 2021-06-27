045154363ba2e16f098f84a3d8fbe69d2d7edf32
t1j <85011304+t1j@users.noreply.github.com>
2021-06-27 15:30:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ window.onload=()=>{
       if (getComputedStyle(ee).display!='none'&&location.ancestorOrigins[0]==undefined) {
         ee.innerHTML=`<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-8870343551793834" data-ad-slot=${vid} data-ad-format="auto" data-full-width-responsive="true"></ins>`
         let spt = document.createElement('script');
-        spt.text = `(adsbygoogle=window.f||[]).push({});`;
+        spt.text = `(adsbygoogle=window.adsbygoogle||[]).push({});`;
         ee.append(spt);
       }
     })
