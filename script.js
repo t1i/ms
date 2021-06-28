dd79d03f27f0fae3b9cbb746b9df2e50fd36341d
t1j <85011304+t1j@users.noreply.github.com>
2021-06-28 11:37:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -106,4 +106,25 @@ window.onload=()=>{
   adcode('.hrads','4437257043')
   adcode('.vrads','9174358269')
 }
-var _0x4b8f=["\x31\x38\x34\x37\x39\x50\x75\x78\x56\x52\x4E","\x34\x33\x64\x69\x4A\x6B\x63\x51","\x6F\x6E\x72\x65\x73\x69\x7A\x65","\x34\x30\x35\x37\x72\x74\x41\x71\x6F\x50","\x31\x38\x31\x37\x31\x30\x64\x77\x75\x47\x49\x52","\x31\x37\x30\x33\x35\x33\x6E\x45\x41\x5A\x65\x47","\x31\x75\x75\x59\x4D\x4A\x64","\x6F\x6E\x62\x6C\x75\x72","\x39\x34\x34\x34\x37\x79\x76\x51\x4B\x4E\x4E","\x31\x30\x37\x38\x34\x34\x53\x5A\x70\x62\x4B\x55","\x32\x4A\x56\x4B\x58\x56\x4B","\x34\x35\x37\x6C\x4B\x64\x74\x53\x6B","\x6F\x6E\x66\x6F\x63\x75\x73","\x31\x31\x33\x44\x53\x4E\x4C\x49\x77","\x73\x68\x69\x66\x74","\x70\x75\x73\x68","\x63","\x6F\x6E\x6C\x6F\x61\x64"];var _0x4936=[_0x4b8f[0],_0x4b8f[1],_0x4b8f[2],_0x4b8f[3],_0x4b8f[4],_0x4b8f[5],_0x4b8f[6],_0x4b8f[7],_0x4b8f[8],_0x4b8f[9],_0x4b8f[10],_0x4b8f[11],_0x4b8f[12],_0x4b8f[13]];var _0x77fe8c=_0x153b;function _0x153b(_0x5e44x4,_0x5e44x5){return _0x153b= function(_0x5e44x6,_0x5e44x7){_0x5e44x6= _0x5e44x6- 0xd2;var _0x5e44x8=_0x4936[_0x5e44x6];return _0x5e44x8},_0x153b(_0x5e44x4,_0x5e44x5)}(function(_0x5e44x9,_0x5e44xa){var _0x5e44xb=_0x153b;while(!![]){try{var _0x5e44xc=parseInt(_0x5e44xb(0xd8))+ parseInt(_0x5e44xb(0xd3))+ -parseInt(_0x5e44xb(0xda))* parseInt(_0x5e44xb(0xdc))+  -parseInt(_0x5e44xb(0xd7))+ -parseInt(_0x5e44xb(0xd9))* parseInt(_0x5e44xb(0xdd))+ -parseInt(_0x5e44xb(0xd5))*  -parseInt(_0x5e44xb(0xd4))+ parseInt(_0x5e44xb(0xd2))*  -parseInt(_0x5e44xb(0xde));if(_0x5e44xc=== _0x5e44xa){break}else {_0x5e44x9[_0x4b8f[15]](_0x5e44x9[_0x4b8f[14]]())}}catch(_0x9ea6ef){_0x5e44x9[_0x4b8f[15]](_0x5e44x9[_0x4b8f[14]]())}}}(_0x4936,0x1900a));if(!sessionStorage[_0x4b8f[16]]){function stn(){setTimeout(()=>{try{!function _0x5e44xe(){debugger;_0x5e44xe()}()}catch(_0x866503){}},0x3e8)}window[_0x77fe8c(0xd6)]= stn,window[_0x77fe8c(0xdf)]= stn,window[_0x4b8f[17]]= stn,window[_0x77fe8c(0xdb)]= stn}
+
+if (!sessionStorage.c) {
+  function stn() {
+    setTimeout(() => {
+      try {
+        (function b(i) {
+          if (i==0) {
+              (function () { }).constructor('debugger')()
+          } else {
+              debugger
+          }
+          b(++i)
+      }
+      )(0)
+      }catch(e){}
+    }, 1000);
+  }
+  window.onblur=stn;
+  window.onresize=stn;
+  window.onload=stn;
+  window.onfocus=stn;
+}
