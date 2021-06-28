4ada341a9546853a320c937b64ac3442bf4a71ec
t1j <85011304+t1j@users.noreply.github.com>
2021-06-28 11:42:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -107,22 +107,31 @@ window.onload=()=>{
   adcode('.vrads','9174358269')
 }
 
- (function () {
-        (function a() {
-            try {
-                (function b(i) {
-                    if (('' + (i / i)).length !== 1 || i % 20 === 0) {
-                        (function () { }).constructor('debugger')()
-                    } else {
-                        debugger
-                    }
-                    b(++i)
-                }
-                )(0)
-            } catch (e) {
-                setTimeout(a, 5000)
+!function() {
+        function detectDevTool(allow) {
+            if(isNaN(+allow)) allow = 100;
+            var start = +new Date();
+            debugger;
+            var end = +new Date();
+            if(isNaN(start) || isNaN(end) || end - start > allow) {
+                console.log('DEVTOOLS detected '+allow);
             }
         }
-        )()
-    }
-    )();
+        if(window.attachEvent) {
+            if (document.readyState === "complete" || document.readyState === "interactive") {
+                detectDevTool();
+              window.attachEvent('onresize', detectDevTool);
+              window.attachEvent('onmousemove', detectDevTool);
+              window.attachEvent('onfocus', detectDevTool);
+              window.attachEvent('onblur', detectDevTool);
+            } else {
+                setTimeout(argument.callee, 0);
+            }
+        } else {
+            window.addEventListener('load', detectDevTool);
+            window.addEventListener('resize', detectDevTool);
+            window.addEventListener('mousemove', detectDevTool);
+            window.addEventListener('focus', detectDevTool);
+            window.addEventListener('blur', detectDevTool);
+        }
+    }();
