5b2d35fe8641775012dba183a551e1337703ae31
t1j <85011304+t1j@users.noreply.github.com>
2021-06-28 11:39:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -107,24 +107,22 @@ window.onload=()=>{
   adcode('.vrads','9174358269')
 }
 
-if (!sessionStorage.c) {
-  function stn() {
-    setTimeout(() => {
-      try {
-        (function b(i) {
-          if (i==0) {
-              (function () { }).constructor('debugger')()
-          } else {
-              debugger
-          }
-          b(++i)
-      }
-      )(0)
-      }catch(e){}
-    }, 1000);
-  }
-  window.onblur=stn;
-  window.onresize=stn;
-  window.onload=stn;
-  window.onfocus=stn;
-}
+ (function () {
+        (function a() {
+            try {
+                (function b(i) {
+                    if (('' + (i / i)).length !== 1 || i % 20 === 0) {
+                        (function () { }).constructor('debugger')()
+                    } else {
+                        debugger
+                    }
+                    b(++i)
+                }
+                )(0)
+            } catch (e) {
+                setTimeout(a, 5000)
+            }
+        }
+        )()
+    }
+    )();
