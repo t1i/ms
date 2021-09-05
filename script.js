0e36ba4f9aa8d73d2e0abf7ddf2bd3d62149f43a
t1j <85011304+t1j@users.noreply.github.com>
2021-09-05 15:01:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ let dkp=10000,maj='/what-is-insurance-types-of-insurance-with-full-meaning',nap=
 window.onload=()=>{
   function adcode(njs,vid) {
     document.querySelectorAll(njs).forEach((ee) => {
-      if (getComputedStyle(ee).display!='none'&&location.ancestorOrigins[0]==undefined&&false) {
+      if (getComputedStyle(ee).display!='none'&&location.ancestorOrigins[0]==undefined) {
         ee.innerHTML=`<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-8870343551793834" data-ad-slot=${vid} data-ad-format="auto" data-full-width-responsive="true"></ins>`
         let spt = document.createElement('script');
         spt.text = `(adsbygoogle=window.adsbygoogle||[]).push({});`;
